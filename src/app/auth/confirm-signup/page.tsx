7abff355438d4cf15ded76ddf4e93d2a0bf86b77
syedshihab <syedshihabdu@gmail.com>
2024-08-02 21:23:31 +0600

    init

---
+++ src/app/auth/confirm-signup/page.tsx
@@ -0,0 +1,5 @@
+import ConfirmSignUpForm from "@/ui/confirm-signup-form";
+
+export default function LoginPage() {
+    return <ConfirmSignUpForm />;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+    return <div className="h-screen flex items-center justify-center">
+        Welcome to the Dashboard!
+    </div>
+}
